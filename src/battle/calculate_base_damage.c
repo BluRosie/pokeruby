9971366a429efa1af8fecae24116180d7e0d9a97
Nathan W <bluroseai@hotmail.com>
2018-08-11 11:20:09 -0400

    add in the attack part of slow start
    slow start also halves attack

--- src/battle/calculate_base_damage.c
+++ src/battle/calculate_base_damage.c
@@ -177,6 +177,7 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
         attack = (150 * attack) / 100;
     if (defender->ability == ABILITY_MARVEL_SCALE && defender->status1)
         defense = (150 * defense) / 100;
+    if (attacker->ability == ABILITY_SLOW_START && gBattleResults.battleTurnCounter <= 4)
         attack = (50 * attack) / 100;
 
     if (type == TYPE_ELECTRIC && AbilityBattleEffects(ABILITYEFFECT_FIELD_SPORT, 0, 0, 0xFD, 0))
