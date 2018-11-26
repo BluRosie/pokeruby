8757915899e7092002b585395296e5e0d1aa0fe1
BluRosie <bluroseai@hotmail.com>
2018-11-26 00:13:11 -0500

    define var for this hack
    i feel like i'm doing 9000 commits for small things

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -172,6 +172,8 @@
 #define VAR_STEVENS_HOUSE_STATE           0x40C6
 #define VAR_OLDALE_STATE                  0x40C7
 
+#define EVENT_VAR_MELTANHACK              0x40C9
+
 // special vars
 // They are commonly used as parameters to commands, or return values from commands.
 #define VAR_SPECIAL_0     0x8000
