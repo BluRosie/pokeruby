fb423171d26da5403b1c2294af028225931f80b3
Nathan Wichers <bluroseai@hotmail.com>
2019-05-21 22:30:01 -0400

    fix up the pokedexentry struct

--- include/pokedex.h
+++ include/pokedex.h
@@ -34,14 +34,14 @@ extern bool8 gUnknown_03005CE8;
 struct PokedexEntry
 {
     /*0x00*/ u8 categoryName[13];
-    /*0x0C*/ u16 height; //in decimeters
-    /*0x0E*/ u16 weight; //in hectograms
-    /*0x10*/ const u8 *description;
-    /*0x1A*/ u16 pokemonScale;
-    /*0x1C*/ u16 pokemonOffset;
-    /*0x1E*/ u16 trainerScale;
-    /*0x20*/ u16 trainerOffset;
-};  /*size = 0x24*/
+    /*0x0D*/ u16 height; //in decimeters
+    /*0x0F*/ u16 weight; //in hectograms
+    /*0x14*/ const u8 *description;
+    /*0x18*/ u16 pokemonScale;
+    /*0x1A*/ u16 pokemonOffset;
+    /*0x1C*/ u16 trainerScale;
+    /*0x1E*/ u16 trainerOffset;
+};  /*size = 0x20*/
 
 extern const struct PokedexEntry gPokedexEntries[];
 
